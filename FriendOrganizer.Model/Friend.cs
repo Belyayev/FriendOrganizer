fbb105ad0af1bf60cd876470f6adf5bb1b17c4e2
Mike <4xgood@gmail.com>
2021-12-19 05:53:40 -0700

    added mock data set for friends list

--- FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer.Model/Friend.cs
@@ -1,8 +1,10 @@
-﻿using System;
-
-namespace FriendOrganizer.Model
+﻿namespace FriendOrganizer.Model
 {
     public class Friend
     {
+        public int Id { set; get; }
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public string Email { set; get; }
     }
 }
---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,21 @@
+﻿using FriendOrganizer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend { FirstName = "Mike", LastName = "Belyayev" };
+            yield return new Friend { FirstName = "Felix", LastName = "Belyayev" };
+            yield return new Friend { FirstName = "Kate", LastName = "Belyayeva" };
+            yield return new Friend { FirstName = "Daniel", LastName = "Belyayev" };
+            yield return new Friend { FirstName = "Victoria", LastName = "Belyayeva" };
+        }
+    }
+}
