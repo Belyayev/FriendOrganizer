eaab962b8dce96484563e1acfa3b633dcf6ba5c8
Mike <4xgood@gmail.com>
2021-12-21 09:53:20 -0700

    added database constrains

--- FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer.Model/Friend.cs
@@ -1,10 +1,16 @@
-﻿namespace FriendOrganizer.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FriendOrganizer.Model
 {
     public class Friend
     {
         public int Id { set; get; }
+        [Required]
+        [StringLength(50)]
         public string FirstName { set; get; }
+        [StringLength(50)]
         public string LastName { set; get; }
+        [StringLength(50)]
         public string Email { set; get; }
     }
 }
